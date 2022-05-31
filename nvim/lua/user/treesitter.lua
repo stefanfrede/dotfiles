ed76c0a7fc23e9570bac4021bfd7907fda8d851f
Stefan Frede <stefan@frede.dev>
2022-05-31 13:38:06 +0200

    feat(lsp): update config

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,27 +1,45 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+	return
 end
 
-configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-  matchup = {
-    enable = true,
-    disable = { "" },  -- list of language that will be disabled
-  },
-}
+configs.setup({
+	ensure_installed = {
+		"astro",
+		"bash",
+		"comment",
+		"css",
+		"dockerfile",
+		"fish",
+		"graphql",
+		"html",
+		"javascript",
+		"jsdoc",
+		"json",
+		"lua",
+		"regex",
+		"scss",
+		"typescript",
+		"vim",
+		"vue",
+	}, -- one of "all", or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	matchup = {
+		enable = true,
+		disable = { "" }, -- list of language that will be disabled
+	},
+})

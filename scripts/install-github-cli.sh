1a22b60c398cea499f3e570766c18ad330865936
Stefan Frede <stefan@frede.dev>
2022-09-17 22:01:30 +0200

    feat(scripts): update github cli install script

--- scripts/install-github-cli.sh
+++ scripts/install-github-cli.sh
@@ -5,14 +5,15 @@ set -euo pipefail
 # Check if gh is installed
 if ! command -v gh >/dev/null 2>&1; then
   # Add official gh packages to sources
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
   # Update sources
-  sudo apt-get -qq update
+  sudo apt update
 
   # Install gh
-  if sudo sudo apt install gh >/dev/null; then
+  if sudo sudo apt install gh -y >/dev/null; then
     echo "Successfully installed gh."
   else
     error "Failed to install gh."

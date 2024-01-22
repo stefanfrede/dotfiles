bcb5d00cd20ae335b3f113895752f3e44d4f525c
Stefan Frede <stefan@frede.dev>
2024-01-22 21:00:12 +0100

    refactor(neovim): adjust install

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -4,46 +4,46 @@ set -euo pipefail
 
 # Check if Neovim is installed
 if ! command -v nvim >/dev/null 2>&1; then
-  # Install build prerequisites
-  sudo apt-get -qq install \
-                   ninja-build \
-                   gettext \
-                   libtool \
-                   libtool-bin \
-                   autoconf \
-                   automake \
-                   cmake \
-                   g++ \
-                   pkg-config \
-                   unzip \
-                   curl \
-                   doxygen
+	# Install build prerequisites
+	sudo apt-get -qq install \
+		ninja-build \
+		gettext \
+		libtool \
+		libtool-bin \
+		autoconf \
+		automake \
+		cmake \
+		g++ \
+		pkg-config \
+		unzip \
+		curl \
+		doxygen
 
-  # Switch to src folder
-  pushd /usr/local/src
+	# Switch to src folder
+	pushd /usr/local/src
 
-  # Check if there is already a neovim folder
-  if [[ ! -d "neovim" ]]; then
-    # Git clone Neovim
-    git clone https://github.com/neovim/neovim
+	# Check if there is already a neovim folder
+	if [[ ! -d "neovim" ]]; then
+		# Git clone Neovim
+		git clone https://github.com/neovim/neovim
 
-    # Change owner
-    sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER neovim
-  fi
+		# Change owner
+		sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER neovim
+	fi
 
-  # cd into folder, get the latest updates, and switch to branch stable
-  cd neovim && git checkout master && git pull && git checkout stable
+	# cd into folder and get the latest updates
+	cd neovim && git pull
 
-  # Clean-up build deps and build source
-  make distclean && make CMAKE_BUILD_TYPE=RelWithDebInfo
+	# Clean-up build deps and build source
+	make distclean && make CMAKE_BUILD_TYPE=Release
 
-  # Install Neovim
-  if sudo make install >/dev/null; then
-    echo "Successfully installed Neovim."
-  else
-    echo "Failed to install Neovim."
-    popd && exit 1
-  fi
+	# Install Neovim
+	if sudo make install >/dev/null; then
+		echo "Successfully installed Neovim."
+	else
+		echo "Failed to install Neovim."
+		popd && exit 1
+	fi
 
-  popd
+	popd
 fi

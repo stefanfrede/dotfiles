5293b508fb9189f1ae66b801b32bbdb4bcf73109
Stefan Frede <stefan@frede.dev>
2024-01-25 10:28:21 +0100

    chore(scripts): clean-up filezilla install

--- scripts/install-filezilla.sh
+++ scripts/install-filezilla.sh
@@ -7,44 +7,44 @@ FILE=FileZilla_Pro_*.tar.bz2
 
 # Check if a FileZilla file is in the Downloads folder
 if compgen -G "${DIR}${FILE}" >/dev/null 2>&1; then
-  pushd /usr/local/src
+	pushd /usr/local/src
 
-  # Remove artefacts
-  for f in FileZilla*; do
-    if [ -d "$f" ]; then
-      sudo rm -rf "$f"
-    else
-      sudo rm -f "$f"
-    fi
-  done
+	# Remove artefacts
+	for f in FileZilla*; do
+		if [ -d "$f" ]; then
+			sudo rm -rf "$f"
+		else
+			sudo rm -f "$f"
+		fi
+	done
 
-  # Copy archive to /usr/local/src
-  sudo find $DIR -name "${FILE}" -exec mv '{}' /usr/local/src/ \;
+	# Copy archive to /usr/local/src
+	sudo find $DIR -name "${FILE}" -exec mv '{}' /usr/local/src/ \;
 
-  # Untar archive
-  for f in FileZilla*; do
-    if [ -f "$f" ]; then
-      sudo tar -xf"$f" 
-    fi
-  done
+	# Untar archive
+	for f in FileZilla*; do
+		if [ -f "$f" ]; then
+			sudo tar -xf"$f"
+		fi
+	done
 
-  # Change owner
-  for f in FileZilla*; do
-    if [ -d "$f" ]; then
-      sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER $f
-    fi
-  done
+	# Change owner
+	for f in FileZilla*; do
+		if [ -d "$f" ]; then
+			sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER $f
+		fi
+	done
 
-  # Remove FileZilla symlink
-  sudo rm /usr/local/bin/filezilla
+	# Remove FileZilla symlink
+	sudo rm /usr/local/bin/filezilla
 
-  # Install FileZilla
-  if sudo ln -s /usr/local/src/FileZilla3/bin/filezilla /usr/local/bin/filezilla >/dev/null; then
-    echo "Successfully installed FileZilla."
-  else
-    error "Failed to install FileZilla."
-    popd && exit 1
-  fi
+	# Install FileZilla
+	if sudo ln -s /usr/local/src/FileZilla3/bin/filezilla /usr/local/bin/filezilla >/dev/null; then
+		echo "Successfully installed FileZilla."
+	else
+		error "Failed to install FileZilla."
+		popd && exit 1
+	fi
 
-  popd
+	popd
 fi
